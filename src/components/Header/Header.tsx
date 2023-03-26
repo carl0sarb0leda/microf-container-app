352b2c99c03cb37c8bf5ca88323b8a1717e1bf57
Carlos <carlos@tradewindow.io>
2023-03-26 18:18:56 +1300

    Add header component + styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {HeaderWrapper} from './header.styled'
+
+export const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h3>MED-APP</h3>
+    </HeaderWrapper>
+  )
+}
---
+++ src/components/Header/header.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 4rem;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
