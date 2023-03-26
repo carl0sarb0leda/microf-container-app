ba9651c099f63294f38fc69dc4a465afca901f93
Carlos <carlos@tradewindow.io>
2023-03-26 18:17:58 +1300

    Add footer component + styles

---
+++ src/components/Footer/footer.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  height: 2rem;
+  background-color: black;
+  color: dimgrey;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`
