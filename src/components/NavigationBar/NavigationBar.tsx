7ec1683f1e201cb361e2fc5f519c598730f68f32
Carlos <carlos@tradewindow.io>
2023-03-26 20:44:47 +1300

    Update navigation bar with icons + styles

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import {NavBarWrapper} from './navigationBar.styled'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+  faLaptopMedical,
+  faChartLine,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 export const NavigationBar = () => {
   return (
     <NavBarWrapper>
-      <NavLink to="/">Dashboard</NavLink>
-      <NavLink to="portal">Portal</NavLink>
-      <NavLink to="about">About</NavLink>
+      <NavLink className="navigation__link" to="/">
+        <FontAwesomeIcon icon={faChartLine} size="2x" />
+        Dashboard
+      </NavLink>
+      <NavLink className="navigation__link" to="portal">
+        <FontAwesomeIcon icon={faLaptopMedical} size="2x" />
+        Portal
+      </NavLink>
+      <NavLink className="navigation__link" to="about">
+        <FontAwesomeIcon icon={faInfoCircle} size="2x" />
+        About
+      </NavLink>
     </NavBarWrapper>
   )
 }
--- src/components/NavigationBar/navigationBar.styled.ts
+++ src/components/NavigationBar/navigationBar.styled.ts
@@ -2,8 +2,21 @@ import styled from 'styled-components'
 
 export const NavBarWrapper = styled.div`
   height: 100%;
-  box-shadow: 0 4px 12px 0 rgba(31, 31, 31, 0.2);
   display: grid;
   align-items: center;
-  width: 6rem;
+  width: 5rem;
+  box-shadow: 0 4px 12px 0 rgba(31, 31, 31, 0.2);
+  background-color: white;
+  font-size: 0.8rem;
+  .navigation__link {
+    text-decoration: none;
+    display: grid;
+    justify-content: center;
+    justify-items: center;
+    color: currentColor;
+    &:hover,
+    &.active {
+      color: darkcyan;
+    }
+  }
 `
