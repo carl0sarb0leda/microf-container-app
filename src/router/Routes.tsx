ea587f6c47a8ae5af295a9186d4041c4123502e5
Carlos <carlos@tradewindow.io>
2023-04-01 18:27:18 +1300

    Update router with portal micro app logic

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import {About, ErrorBoundary, NotFound} from 'components'
-import {Dashboard, Layout} from 'containers'
+import {Dashboard, Layout, MicroApp} from 'containers'
 
 const Routes = () => {
   const router = createBrowserRouter([
@@ -18,6 +18,17 @@ const Routes = () => {
         {
           path: '/about',
           element: <About />,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: '/portal/*',
+          element: (
+            <MicroApp
+              name="portal-app"
+              containerId="portal-app-container"
+              host="http://localhost:3302"
+            />
+          ),
           errorElement: <ErrorBoundary />,
         },
         {
