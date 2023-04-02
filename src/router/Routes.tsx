ffb121266912cdcd1d9d5ba02608caa59a9bcb8c
Carlos <carlos@tradewindow.io>
2023-04-03 01:06:15 +1200

    Update router logic test

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -6,46 +6,41 @@ import {PortalApp} from 'lib/constants'
 import {customLoader} from 'utils/fn-helper'
 
 const Routes = () => {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <Layout />,
-        errorElement: <ErrorBoundary />,
-        children: [
-          {
-            path: '/',
-            element: <Dashboard />,
-            errorElement: <ErrorBoundary />,
-          },
-          {
-            path: '/about',
-            element: <About />,
-            errorElement: <ErrorBoundary />,
-          },
-          {
-            path: '/portal/*',
-            element: (
-              <MicroApp
-                name={PortalApp.NameSpace}
-                containerId={PortalApp.ContainerId}
-                host={PortalApp.Host}
-              />
-            ),
-            errorElement: <ErrorBoundary />,
-          },
-          {
-            path: '*',
-            element: <NotFound />,
-            loader: customLoader,
-          },
-        ],
-      },
-    ],
+  const router = createBrowserRouter([
     {
-      basename: 'microf-container-app',
+      path: '/',
+      element: <Layout />,
+      errorElement: <ErrorBoundary />,
+      children: [
+        {
+          path: '/',
+          element: <Dashboard />,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: '/about',
+          element: <About />,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: '/portal/*',
+          element: (
+            <MicroApp
+              name={PortalApp.NameSpace}
+              containerId={PortalApp.ContainerId}
+              host={PortalApp.Host}
+            />
+          ),
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: '*',
+          element: <NotFound />,
+          loader: customLoader,
+        },
+      ],
     },
-  )
+  ])
   return <RouterProvider router={router} />
 }
 
