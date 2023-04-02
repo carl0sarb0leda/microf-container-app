45e670e8f665f8953d76b3755a3aa30936582615
Carlos <carlos@tradewindow.io>
2023-04-02 23:56:23 +1200

    Add custom loader helper

---
+++ src/utils/fn-helper.ts
@@ -0,0 +1,8 @@
+import {redirect} from 'react-router-dom'
+
+export const customLoader = async () => {
+  //validation for gh-pages deployment
+  const pathname = window.location.pathname
+  if (pathname === '/microf-container-app') return redirect('/')
+  return null
+}
